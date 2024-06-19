e320a1c018a2439ebb346811a1b7834e647ada47
marialuiza <maria_luiza1598@hotmail.com>
2024-06-18 21:54:24 -0300

    changing cypress config

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -4,8 +4,6 @@ module.exports = defineConfig({
     fixturesFolder: false,
     e2e: {
         setupNodeEvents(on, config) {
-            return require('./cypress/plugins/index.js')(on, config)
         },
-        baseUrl: 'http://localhost:1234',
     },
 })
